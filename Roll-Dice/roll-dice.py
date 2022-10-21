8c7447d56f87d1e8e0c65044cbe444a55aa2f2fe
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-10-21 23:05:56 +0600

    python coding based project 1st day

---
+++ Roll-Dice/roll-dice.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+root = Tk()
+root.geometry("800x550")
+root.title("Roll Rice Created By Rc Mehedi Hasan")
+
+label = Label(root,text="",font=("times",260))
+
+def roll():
+    dice = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
+    label.configure(text=(f'{random.choice(dice)}{random.choice(dice)}'))
+    label.pack()
+
+button = Button(root,text="LET's Rolling.......",width=40,height=5,font=10,bg="white",bd=2,command=roll)
+button.pack(padx=10,pady=10)
+root.mainloop()
