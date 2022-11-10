a7afaab449a10f8690160fd8b9b30d3064f72190
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-11-10 15:06:55 +0600

    word replace project

---
+++ Word-Replacement-Program/Word-Replacement-Program.py
@@ -0,0 +1,7 @@
+def replace_word():
+
+    Write = "Hi Dear,And How are You?"
+    word_to_replace = input("Enter The Word To Replace : ")
+    word_replacement = input("Enter The Word Replacement :")
+    print(Write.replace(word_to_replace,word_replacement))
+replace_word()
