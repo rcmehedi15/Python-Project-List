6aa60a0ff983a03924b367fee92f25e59b07e657
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-11-11 00:31:47 +0600

    Internet Speed checker

---
+++ internet-speed-checker/internet-speed-checker.py
@@ -0,0 +1,13 @@
+from turtle import speed
+import speedtest
+
+ST = speedtest.Speedtest()
+
+print("Download Speed : ")
+print(ST.download())
+
+print("\nUploading Speed : ")
+print(ST.upload())
+
+print("\nPing: ")
+print(ST.results.ping)
